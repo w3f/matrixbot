c1d8abe283bc5e8a698a3931cf49c06d24bb08a8
Federico Gimenez <federico.gimenez@gmail.com>
2019-03-30 18:16:55 +0100

    make sure to remove new lines

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 /scripts/deploy.sh helm \
-                   --set accessToken=${MATRIXBOT_ACCESS_TOKEN} \
-                   --set image.tag=${CIRCLE_TAG} \
-                   --set alertServiceId=${MATRIXBOT_ALERT_SERVICE_ID} \
+                   --set accessToken="${MATRIXBOT_ACCESS_TOKEN}" \
+                   --set image.tag="${CIRCLE_TAG}" \
+                   --set alertServiceId="${MATRIXBOT_ALERT_SERVICE_ID}" \
                    matrixbot \
                    w3f/matrixbot
