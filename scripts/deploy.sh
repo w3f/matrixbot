b4a522715132c97028bf025a204b05f374449fe6
Federico Gimenez <federico.gimenez@gmail.com>
2019-03-30 12:09:43 +0100

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@
 
 /scripts/deploy.sh helm \
                    --set accessToken=${MATRIXBOT_ACCESS_TOKEN} \
-                   --set image.tag=${CIRCLE_TAG}
+                   --set image.tag=${CIRCLE_TAG} \
                    matrixbot \
                    w3f/matrixbot
