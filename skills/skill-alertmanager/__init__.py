8846f55de0b655d090901394d2485ea07413aaf5
Federico Gimenez <federico.gimenez@gmail.com>
2019-04-03 14:33:23 +0200

    parsing payload

--- skills/skill-alertmanager/__init__.py
+++ skills/skill-alertmanager/__init__.py
@@ -1,23 +1,28 @@
 from aiohttp.web import Request
 
+from opsdroid.skill import Skill
 from opsdroid.matchers import match_webhook
 from opsdroid.events import Message
 
+import json
 import logging
 
 _LOGGER = logging.getLogger(__name__)
 
-@match_webhook('webhook')
-async def mywebhookskill(opsdroid, config, message):
-    if type(message) is not Message and type(message) is Request:
-        request = await message.post()
+class MySkill(Skill):
+    @match_webhook('webhook')
+    async def mywebhookskill(self, opsdroid, config, message):
+        if type(message) is not Message and type(message) is Request:
+            request = await message.post()
 
-        message = Message("",
-                          None,
-                          opsdroid.default_connector,
-                          config.get("room", opsdroid.default_connector.default_room))
+            message = Message("",
+                              None,
+                              opsdroid.default_connector,
+                              config.get("room", opsdroid.default_connector.default_room))
 
-        for key in request.keys():
-            _LOGGER.debug('payload received: ' + key)
+            for key in request.keys():
+                _LOGGER.debug('payload received: ' + key)
 
-        await message.respond('Hey')
+                payload = json.loads(key)
+
+                await message.respond('Hey, ' + payload["version"])
