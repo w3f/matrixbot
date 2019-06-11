72d4fccfd00fb02e73e320edf14a801b38d8f29b
Federico Gimenez <federico.gimenez@gmail.com>
2019-06-11 18:01:57 +0200

    initialize variables in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+if [ "${ENVIRONMENT}" = "w3f" ]; then
+    MATRIXBOT_USER="${W3F_MATRIXBOT_USER}"
+    MATRIXBOT_PASSWORD="${W3F_MATRIXBOT_PASSWORD}"
+    MATRIXBOT_ROOM_ID="${W3F_MATRIXBOT_ROOM_ID}"
+else
+    MATRIXBOT_USER="${COMM_MATRIXBOT_USER}"
+    MATRIXBOT_PASSWORD="${COMM_MATRIXBOT_PASSWORD}"
+    MATRIXBOT_ROOM_ID="${COMM_MATRIXBOT_ROOM_ID}"
+fi
+
 /scripts/deploy.sh -c ${ENVIRONMENT} -t helm -- \
                    --set botUser="${MATRIXBOT_USER}" \
                    --set botPassword="${MATRIXBOT_PASSWORD}" \
