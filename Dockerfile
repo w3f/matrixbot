b1d8ad504e491c467e65aec92e123d65e30be031
Federico Gimenez <federico.gimenez@gmail.com>
2019-12-22 21:11:54 +0100

    bump matrixbot dep

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM opsdroid/opsdroid:v0.14.1
+FROM opsdroid/opsdroid:v0.17.0
 
 RUN adduser -D -g '' opsdroid
 
--- skills/skill-eventmanager/__init__.py
+++ skills/skill-eventmanager/__init__.py
@@ -6,11 +6,8 @@ from opsdroid.events import Message
 
 import json
 import logging
-import os
 import pprint
 
-LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO').upper()
-logging.basicConfig(level=LOG_LEVEL)
 _LOGGER = logging.getLogger(__name__)
 
 
